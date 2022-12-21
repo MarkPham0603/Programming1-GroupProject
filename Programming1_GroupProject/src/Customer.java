555b7b7c8b038a298a340fe78aac83d7ae7ffd7c
S3929256 <102713221+MarkPham0603@users.noreply.github.com>
2022-12-21 17:15:01 +0700

    add constructor, getter and setter for Customer class

--- Programming1_GroupProject/src/Customer.java
+++ Programming1_GroupProject/src/Customer.java
@@ -1,2 +1,6 @@
-public class Customer {
+public class Customer extends User {
+
+    public Customer(String username, String password, String fullname, String email, String address, int phoneNumber) {
+        super(username, password, fullname, email, address, phoneNumber);
+    }
 }
