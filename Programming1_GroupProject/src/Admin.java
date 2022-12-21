dd4938bee7e2ea2bac99c703052162d2686f150b
S3929256 <102713221+MarkPham0603@users.noreply.github.com>
2022-12-21 17:14:14 +0700

    add constructor, getter and setter for Admin class

--- Programming1_GroupProject/src/Admin.java
+++ Programming1_GroupProject/src/Admin.java
@@ -1,2 +1,6 @@
-public class Admin {
+public class Admin extends User{
+
+    public Admin(String username, String password, String fullname, String email, String address, int phoneNumber) {
+        super(username, password, fullname, email, address, phoneNumber);
+    }
 }
