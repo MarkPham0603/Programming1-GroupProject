158166c25a217becd4b474b1de9060545b8f2b58
S3929256 <102713221+MarkPham0603@users.noreply.github.com>
2022-12-21 17:13:27 +0700

    add constructor, getter and setter for Users class

--- Programming1_GroupProject/src/User.java
+++ Programming1_GroupProject/src/User.java
@@ -1,2 +1,61 @@
 public class User {
+    private String username, password, fullname, email, address;
+    private int phoneNumber;
+
+    public User(String username, String password, String fullname, String email, String address, int phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
