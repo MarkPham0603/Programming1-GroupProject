9c1da8bee4094e9f2fd9c6b3cdb4354412915a0a
Kien Chau <76087194+chautkien@users.noreply.github.com>
2022-12-22 19:43:52 +0700

    Update Product.java

--- Programming1_GroupProject/src/Product.java
+++ Programming1_GroupProject/src/Product.java
@@ -1,2 +1,46 @@
 public class Product {
+    private int id;
+    private String name;
+    private float price;
+    private String category;
+
+    public Product(int id, String name,float price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price){
+        this.price = price;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public void setCategory() {
+        this.category =category;
+    }
+
 }
