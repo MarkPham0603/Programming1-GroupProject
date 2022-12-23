76d3c12c8f9ab93dd7978092b66e075d1d6f7b29
LeBon985 <120450276+LeBon985@users.noreply.github.com>
2022-12-24 01:19:49 +0700

    Update Order.java

--- Programming1_GroupProject/src/Order.java
+++ Programming1_GroupProject/src/Order.java
@@ -1,2 +1,41 @@
+import java.util.Date;
+
 public class Order {
+    private String orderID;
+    private Date Datecreated;
+    private String status;
+
+
+    public Order(String orderID, Date datecreated, String status) {
+        this.orderID = orderID;
+        Datecreated = datecreated;
+        this.status = status;
+    }
+
+    public Order() {
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public Date getDatecreated() {
+        return Datecreated;
+    }
+
+    public void setDatecreated(Date datecreated) {
+        this.Datecreated = datecreated;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
