92108279785ae0b99befc08e26b376d66f803bc1
LeBon985 <120450276+LeBon985@users.noreply.github.com>
2022-12-24 01:20:17 +0700

    Update OrderService.java

--- Programming1_GroupProject/src/OrderService.java
+++ Programming1_GroupProject/src/OrderService.java
@@ -1,2 +1,55 @@
-public class OrderService {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Orderservice extends Order {
+    public Orderservice(String orderID, Date datecreated, String status) {
+        super(orderID, datecreated, status);
+    }
+
+    private List<Order> listofOrder;
+
+    public Orderservice() {
+        this.listofOrder = new ArrayList<>();
+    }
+
+    public Orderservice(List<Order> listofOrder) {
+        this.listofOrder = listofOrder;
+    }
+
+    public List<Order> getListofOrder() {
+        return listofOrder;
+    }
+
+    public int addOrder(Order n) {
+        this.listofOrder.add(n);
+        return count();
+    }
+
+    public int count() {
+        return this.listofOrder.size();
+    }
+
+    public Order getOrder(int index){
+        if(index < 0 || index >= count()){
+            return null;
+        }
+        return this.listofOrder.get(index);
+    }
+
+    public boolean removeOrder(String orderID){
+        int index = -1;
+        for(int i = 0, n= count(); i<n; i++) {
+            if(this.listofOrder.get(i).getOrderID() == orderID ){
+                index = i;
+                break;
+            }
+        }
+        if (index != -1){
+            this.listofOrder.remove(index);
+            return true;
+        }
+        return false;
+    }
 }
+
