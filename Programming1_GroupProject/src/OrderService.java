0c547df778f7580f1fcfb771b044fb49182815b8
S3929256 <102713221+MarkPham0603@users.noreply.github.com>
2023-01-15 15:09:14 +0700

    Final update

--- Programming1_GroupProject/src/OrderService.java
+++ Programming1_GroupProject/src/OrderService.java
@@ -1,92 +1,220 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Collections;
-import java.util.Comparator;
+/*
+  RMIT University Vietnam
+  Course: COSC2081 Programming 1
+  Semester: 2022C
+  Assessment: Assignment 3
+  Author: Pham Minh Hoa,Chau The Kien
+  ID: s3929256, s3790421
+  Acknowledgement: Tom huynh https://github.com/TomHuynhSG/COSC2081-Lab-Solutions
+                   My old project https://github.com/Devtorious/Programming-1
+*/
+import java.io.*;
+import java.util.*;
 
-class OrderService {
-    private List<Order> orders;
-    private int orderId;
-    private ProductService productService; // an instance of ProductService class to interact with products
-    
-        public int createOrder(Customer customer, List<Product> products) {
-        // calculate total price for the products and create new order
-        double totalPrice = 0;
-        for(Product product: products) {
-            totalPrice += product.getPrice();
-        }
-        Order order = new Order(++orderId, customer, products, "New", totalPrice, 0);
-        orders.add(order);
-        return orderId;
-    }
-    
-    public void updateOrder(int orderId, List<Product> productsToAdd, List<Product> productsToRemove) {
-        // add or remove products from the order
-        Order order = getOrderById(orderId);
-        if (order != null) {
-            for(Product product: productsToAdd) {
-                order.getProducts().add(product);
-                order.setTotalPrice(order.getTotalPrice() + product.getPrice());
+import static java.lang.String.format;
+
+public class OrderService {
+    //Create an ArrayList to store orderList
+    public static List<OrderForm>  orderList = new ArrayList<OrderForm>();
+
+    public void createOrder() {
+        //A method to create new order
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        //Making order id using random class
+        String id = ("Odr" + random.nextInt(10) + "-" + random.nextInt(1000));
+        System.out.println("Enter your name: ");
+        String cname = sc.nextLine();
+        System.out.println("Enter your phone number: ");
+        String cphone = sc.nextLine();
+        System.out.println("Enter product you want to buy: ");
+        String pname = sc.nextLine();
+        String pcategory = "";
+        double pprice = 0;
+        String status = "Confirming";
+
+        boolean loop = true;
+        Product item = null;
+        //Use do while loop to scan for the product name, then set the pcategory and pprice equal to Product category and price
+        do {
+            for (Product p : ProductService.productList) {
+                if (p.getName().equals(pname)){
+                    item = p;
+                    loop = false;
+                }
             }
-            for(Product product: productsToRemove) {
-                order.getProducts().remove(product);
-                order.setTotalPrice(order.getTotalPrice() - product.getPrice());
-            }
-        }
-    }
-    
-    public double viewOrderPrice(int orderId) {
-        // view the total price of all items in the order
-        Order order = getOrderById(orderId);
-        if (order != null) {
-            return order.getTotalPrice();
-        }
-        return 0;
-    }
-    
-    public Order getOrderById(int orderId) {
-        // helper method to retrieve an order by its id
-        for (Order order : orders) {
-            if (order.getOrderId() == orderId) {
-                return order;
-            }
-        }
-        return null;
-    }
-     public void approveOrder(int orderId) {
-        // change the status of an order to "delivered"
-        Order order = getOrderById(orderId);
-        if (order != null) {
-            order.setStatus("Delivered");
-        }
+            pcategory = item.getCategory();
+            pprice = item.getPrice();
+        } while (loop == true);
+        //Create new OrderForm and add to orderlist
+        OrderForm odrF = new OrderForm(id,cname,cphone,pname,pcategory,status,pprice);
+        orderList.add(odrF);
     }
 
-    public void viewOrderStatus(int orderId) {
-        // view the status of an order
-        Order order = getOrderById(orderId);
-        if (order != null) {
-            System.out.println("Order Status: " + order.getStatus());
+    public void showOrderList() {
+        //Show product list
+        System.out.println("Order List");
+        //Make columns
+        String column = format("%s%15s%15s%15s%15s%15s", "ID", "Customer", "Phone", "Product", "Price", "Status");
+        System.out.println(column);
+        for (OrderForm or : orderList) {
+            //Making rows
+            String row = format("%s%15s%15s%15s%15f%15s",or.getID(),or.getCname(),or.getCphone(),
+                    or.getPname(),or.getPprice(),or.getStatus());
+            System.out.println(row);
         }
     }
-    
-    public void viewAllOrders() {
-        // view all the orders
-        for (Order order : orders) {
-            System.out.println("Order ID: " + order.getOrderId());
-            System.out.println("Customer: " + order.getCustomer().getName());
-            System.out.println("Products: " + order.getProducts());
-            System.out.println("Total Price: " + order.getTotalPrice());
-            System.out.println("Discount: " + order.getDiscount());
-            System.out.println("Status: " + order.getStatus());
+
+    public void insertOrderArray() throws IOException {
+        //Save all the OrderForm objects to a text file
+        PrintWriter pw = new PrintWriter(new FileWriter("src/Orders.txt",true));
+
+        for (int i = 0; i < orderList.size(); i++) {
+            pw.println(orderList.get(i) + " ");
         }
+
+        pw.flush();
+        pw.close();
     }
-        public void sortProductsByName(Order order) {
-        Collections.sort(order.getProducts(), new Comparator<Product>() {
-            @Override
-            public int compare(Product p1, Product p2) {
-                return p1.getTitle().compareTo(p2.getTitle());
+
+    public void readOrderfile() throws Exception {
+        File file = new File("src/Orders.txt");
+        Scanner inputfile;
+        String id, cname, cphone, pname, pcategory, status, line;
+        double pprice;
+        //Check if the file is existed
+        if (file.exists()) {
+            inputfile = new Scanner(file);
+            //while loop to scan
+            while (inputfile.hasNext()) {
+                line = inputfile.nextLine();
+                //Tokenize a line of text using the deliminator ", " by utilizing the StringTokenizer object
+                StringTokenizer inReader = new StringTokenizer(line,",");
+                //Check to make sure that each line has precisely three tokens.
+                if (inReader.countTokens() != 7) {
+                    throw new Exception("Invalid Input Format");
+                } else {
+                    //Tokens are always 7 per line, therefore id, cname, cphone, pname, pcategory, status and pprice should be assigned in turn.
+                    id = inReader.nextToken();
+                    cname = inReader.nextToken();
+                    cphone = inReader.nextToken();
+                    pname = inReader.nextToken();
+                    pcategory = inReader.nextToken();
+                    status = inReader.nextToken();
+                    pprice = Double.parseDouble(inReader.nextToken());
+
+                    orderList.add(new OrderForm(id,cname,cphone,pname,pcategory,status,pprice));
+                }
             }
-        });
+        }
+    }
+
+    public void searchOrder() {
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        do {
+            System.out.println("Enter ID order: ");
+            String id = sc.nextLine();
+            OrderForm order= null;
+            for (OrderForm o : orderList) {
+                if (o.getID().equals(id)) {
+                    order = o;
+                }
+            }
+            if (order == null) {
+                loop = true;
+                System.out.println("Wrong ID!!!");
+                break;
+            } else if (order != null) {
+                loop = false;
+            }
+
+            System.out.println("Order Information");
+            System.out.println("ID: " + order.getID() +  " CName: " + order.getCname() + " Cphone: " + order.getCphone()
+                    + " Pname: "  + order.getPname() + " Pcategory: " + order.getPcategory() + " Status: " + order.getStatus() +  " Price: " + order.getPprice());
+
+        } while (loop == true);
+    }
+
+    public void calculateOrder() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter customer name: ");
+        String name = sc.nextLine();
+        double total = 0;
+        //Scan through the orderList to all the orderForm of input name
+        for (int i = 0; i < orderList.size(); i++) {
+            if (name.equals(orderList.get(i).getCname())) {
+                //Calculate the total of all order
+                total = total + orderList.get(i).getPprice();
+            }
+        }
+        System.out.println("Your total price: " + total);
+        double finalTotal = 0;
+        double discount;
+        for (int i = 0; i < UserLogin.userList.size(); i++) {
+            //Checking membership to get discount
+            if (UserLogin.userList.get(i).getMembership().equals("Silver") && UserLogin.userList.get(i).getFullname().equals(name)) {
+                System.out.println("You have 5% discount as Silver membership");
+                discount = total * 5/100;
+                finalTotal = total - discount;
+                System.out.println("Discount: " + discount);
+            } else if (UserLogin.userList.get(i).getMembership().equals("Gold") && UserLogin.userList.get(i).getFullname().equals(name)) {
+                System.out.println("You have 10% discount as Gold membership");
+                discount = total * 10/100;
+                finalTotal = total - discount;
+                System.out.println("Discount: " + discount);
+            } else if (UserLogin.userList.get(i).getMembership().equals("Platinum") && UserLogin.userList.get(i).getFullname().equals(name)) {
+                System.out.println("You have 15% discount as Platinum membership");
+                discount = total * 15/100;
+                finalTotal = total - discount;
+                System.out.println("Discount: " + discount);
+            }
+        }
+        System.out.println("Final total price: " + finalTotal);
+    }
+
+    public void storeRevenue() {
+        double day_total = 0;
+        //Scan all OrderList and calculate day total
+        for (int i = 0; i < orderList.size(); i++) {
+            day_total = day_total + orderList.get(i).getPprice();
+        }
+        System.out.println("Store revenue today is: " + day_total);
+    }
+
+    public void setOrderStatus() {
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        do {
+            System.out.println("Enter ID order: ");
+            String id = sc.nextLine();
+            OrderForm order= null;
+            for (OrderForm o : orderList) {
+                if (o.getID().equals(id)) {
+                    order = o;
+                }
+            }
+            if (order == null) {
+                loop = true;
+                System.out.println("Wrong ID!!!");
+                break;
+            } else if (order != null) {
+                loop = false;
+            }
+
+            System.out.println("Order Information");
+            System.out.println("ID: " + order.getID() +  " CName: " + order.getCname() + " Cphone: " + order.getCphone()
+                    + " Pname: "  + order.getPname() + " Pcategory: " + order.getPcategory() + " Status: " + order.getStatus() +  " Price: " + order.getPprice());
+            //Scan the orderList
+            for (int i =0; i < orderList.size(); i++) {
+                if (orderList.get(i).getID().equals(id)) {
+                    //Set new order Status
+                    System.out.println("Set Order Status: ");
+                    String status = sc.nextLine();
+                    orderList.get(i).setStatus(status);
+                }
+            }
+
+        } while (loop == true);
     }
 }
