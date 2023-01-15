1f1d9132a0b941baec37fbf558c1b3efc3fbbd7f
S3929256 <102713221+MarkPham0603@users.noreply.github.com>
2023-01-15 15:11:19 +0700

    Final update

--- Programming1_GroupProject/src/UserLogin.java
+++ Programming1_GroupProject/src/UserLogin.java
@@ -1,10 +1,21 @@
+/*
+  RMIT University Vietnam
+  Course: COSC2081 Programming 1
+  Semester: 2022C
+  Assessment: Assignment 3
+  Author: Pham Minh Hoa,Chau The Kien
+  ID: s3929256, s3790421
+  Acknowledgement: Tom huynh https://github.com/TomHuynhSG/COSC2081-Lab-Solutions
+                   My old project https://github.com/Devtorious/Programming-1
+*/
 import java.io.*;
 import java.util.*;
 
 public class UserLogin {
     public static List<User> userList = new ArrayList<User>();
 
-    public void register() throws IOException {
+    public void register() {
+        //Create new user
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String fullname = sc.nextLine();
@@ -17,16 +28,16 @@ public class UserLogin {
         System.out.println("Enter your password: ");
         String password = sc.nextLine();
         System.out.println("Enter your phone number: ");
-        String membership = "none";
+        String membership = "Regular";
         String phoneNumber = sc.nextLine();
-
+        //Create new User objects by using all inputs above
         User ur = new User(username,password,fullname,email,address,membership,phoneNumber);
-
+        //Add object to array list
         userList.add(ur);
     }
 
     public void insertArray() throws IOException {
-        PrintWriter pw = new PrintWriter(new FileWriter("src/test.txt",true));
+        PrintWriter pw = new PrintWriter(new FileWriter("src/customerinfo.txt",true));
 
         for (int i = 0; i < userList.size(); i++) {
             pw.println(userList.get(i) + " ");
@@ -37,7 +48,7 @@ public class UserLogin {
     }
 
     public void readtxtFile() throws Exception {
-        File file = new File("src/test.txt");
+        File file = new File("src/customerinfo.txt");
         Scanner inputfile;
         String username,password,fullname,email,address,membership,phoneNumber,line;
 
@@ -63,6 +74,7 @@ public class UserLogin {
     }
 
     public void Login() {
+        //Login form
         Scanner sc = new Scanner(System.in);
         boolean loop = true;
         do {
@@ -79,9 +91,7 @@ public class UserLogin {
             }
             if (account == null) {
                 loop = true;
-                System.out.println(account);
             } else if (account != null) {
-                System.out.println(account);
                 loop = false;
             }
         } while (loop == true);
